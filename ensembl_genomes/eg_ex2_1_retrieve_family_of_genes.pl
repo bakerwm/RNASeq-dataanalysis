1ed200f43d5946ed8291594ffb4dc8ecfd741145
bakerwm <wangmcas@gmail.com>
2015-01-30 17:05:28 +0800

    update scripts

--- ensembl_genomes/eg_ex2_1_retrieve_family_of_genes.pl
+++ ensembl_genomes/eg_ex2_1_retrieve_family_of_genes.pl
@@ -10,13 +10,12 @@ use Data::Dumper;
 
 # Retrieve the family of a given gene
 
-my $lookup = Bio::EnsEMBL::LookUp->new();
-
 #my $nom = 'mycobacterium_tuberculosis_h37rv_gca_000195955_2';
 #my $input_gene = 'Rv2925c'; # RNase III
-
 my $nom = 'escherichia_coli_str_k_12_substr_mg1655';
 my $input_gene = 'b0344';
+
+my $lookup = Bio::EnsEMBL::LookUp->new();
 
 # load gene adaptor
 my $dba = $lookup->get_by_name_exact($nom);
@@ -25,37 +24,33 @@ my $gene_adaptor = $dba->get_GeneAdaptor();
 
 my $gene = $gene_adaptor->fetch_by_stable_id($input_gene); # lacZ
 
-## load member/family adaptors
-#my $reg = 'Bio::EnsEMBL::Registry';
-#$reg->load_registry_from_db(
-#        -host => 'mysql-eg-publicsql.ebi.ac.uk',
-#        -port => '4157',
-#        -user => 'anonymous'
-#        );
-#
-#my $gene_member_adaptor = $reg->get_adaptor(
-#        'bacteria', 'compara', 'GeneMember'
-#        );
-#
-#my $family_adaptor = $reg->get_adaptor(
-#        'bacteria', 'compara', 'Family'
-#        );
-#
-#my $gene_member = $gene_member_adaptor->fetch_by_stable_id( $gene->stable_id() );
-#
-#my $families = $family_adaptor->fetch_all_by_GeneMember($gene_member);
-
-# load compara adaptor
-my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
+# load Registry
+my $registry = 'Bio::EnsEMBL::Registry';
+$registry->load_registry_from_db(
         -host => 'mysql-eg-publicsql.ebi.ac.uk',
         -port => '4157',
-        -user => 'anonymous',
-        -dbname => 'ensembl_compara_bacteria_25_78'
+        -user => 'anonymous'
         );
 
-my $member = $compara_dba->get_GeneMemberAdaptor()->fetch_by_stable_id($input_gene);
+my $member_adaptor = $registry->get_adaptor('bacteria', 'compara', 'GeneMember');
 
-my $families = $compara_dba->get_FamilyAdaptor()->fetch_all_by_GeneMember($member);
+my $family_adaptor = $registry->get_adaptor('bacteria', 'compara', 'Family');
+
+my $member = $member_adaptor->fetch_by_stable_id($input_gene);
+
+my $families = $family_adaptor->fetch_all_by_GeneMember($member);
+
+## load compara adaptor
+#my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
+#        -host => 'mysql-eg-publicsql.ebi.ac.uk',
+#        -port => '4157',
+#        -user => 'anonymous',
+#        -dbname => 'ensembl_compara_bacteria_25_78'
+#        );
+#
+#my $member = $compara_dba->get_GeneMemberAdaptor()->fetch_by_stable_id($input_gene);
+
+#my $families = $compara_dba->get_FamilyAdaptor()->fetch_all_by_GeneMember($member);
 
 # loop out
 print "Retrieve the family of input gene: ", $input_gene, "\n", "From genome: ", $nom, "\n\n";
@@ -64,15 +59,5 @@ print "Family", "\t", "Family_description", "\n";
 foreach my $family (@{$families}){
     # this family
     print $family->stable_id(), "\t", $family->description(), "\n";
-
-#    # all members in this family
-#    my $all_members = $family->get_all_Members();
-#    my $count = 1;
-#    foreach my $member (@{$all_members}){
-#        print "$count ";
-#        print $member->source_name, " ", $member->stable_id, " ", "\n";
-#        $count ++;
-#   }
-#    print "\n";
 }
 
--- ensembl_genomes/eg_ex2_3_retrieve_genes_belonging_to_a_family_of_species.pl
+++ ensembl_genomes/eg_ex2_3_retrieve_genes_belonging_to_a_family_of_species.pl
@@ -2,35 +2,20 @@
 use strict;
 use warnings;
 
-use Bio::EnsEMBL::Registry;
+#use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::LookUp;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 use Data::Dumper;
 
-# find all genomes that descendants of a specified node (as a filter)
+my $input_family = 'MF_00395';
+my $taxid = 1219; #Prochlorococcus marinus
 
+# find all genomes that descendants of a specified node (as a filter)
 my $lookup = Bio::EnsEMBL::LookUp->new();
 
-my $input_family = 'MF_00395';
-
-my $taxid = 1219; # Prochlorococcus marinus
 my $branches = $lookup->get_all_by_taxon_branch($taxid);
 my %target_species = map {$_->species() => $_ } @{$branches};
-
-# load family adaptors
-#my $reg = 'Bio::EnsEMBL::Registry';
-#$reg->load_registry_from_db(
-#        -host => 'mysql-eg-publicsql.ebi.ac.uk',
-#        -port => '4157',
-#        -user => 'anonymous'
-#        );
-#
-#my $family_adaptor = $reg->get_adaptor(
-#        'bacteria', 'compara', 'Family'
-#        );
-#
-#my $family = $family_adaptor->fetch_by_stable_id($input_family);
 
 # load compara adaptors
 my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
@@ -56,11 +41,8 @@ foreach my $member (@{$members}){
     if(defined $member_dba){
         my $gene_adaptor = $member_dba->get_GeneAdaptor();
         my $gene = $gene_adaptor->fetch_by_stable_id($member->gene_member()->stable_id());
-        
-        my $out_gene_name = (defined $gene->stable_id())?$gene->stable_id():"-";
-        my $out_gene_ext  = (defined $gene->external_name())?$gene->external_name():"-";
-        my $out_species = (defined $member_dba->species())?$member_dba->species():"-";
-        print $out_gene_name, "\t", $out_gene_ext, "\t", $out_species, "\n";
+        my $gene_external_name  = (defined $gene->external_name())?$gene->external_name():"-";
+        print $gene->stable_id(), "\t", $gene_external_name, "\t", $member_dba->species(), "\n";
         $member_dba->dbc()->disconnect_if_idle();
     }
 }
